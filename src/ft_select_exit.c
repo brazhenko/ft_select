6fc53fa613766b6743ab04d311acff3f7e12f533
Gillam Hazrak zo loraq <ghazrak-@e1r1p3.21-school.ru>
2019-02-26 06:18:17 +0300

    fix exit (esc)

--- src/ft_select_exit.c
+++ src/ft_select_exit.c
@@ -3,21 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_select_exit.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lreznak- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ghazrak- <ghazrak-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/24 07:48:36 by lreznak-          #+#    #+#             */
-/*   Updated: 2019/02/24 07:50:13 by lreznak-         ###   ########.fr       */
+/*   Updated: 2019/02/26 06:17:27 by ghazrak-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_select.h"
+#include "../inc/ft_select.h"
 
 void			ft_select_exit(void)
 {
 	char buf[1024];
-	char *termtype=getenv("TERM");
-	int ok=tgetent(buf,termtype);
-	char *test1=tgetstr("cl",0); /* очистка экрана */
-	printf("%s", test1);
+	char *termtype = getenv("TERM");
+	int ok = tgetent(buf,termtype);
+	char *test1 = tgetstr("cl",0); /* очистка экрана */
+	char *test2 = tgetstr("ve",0);
+	char *test3 = tgetstr("te",0);
+	printf("%s%s%s", test2, test3, test1);
 	exit(EXIT_SUCCESS);
 }
